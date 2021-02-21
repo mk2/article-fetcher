d4460920d70c9d1b708dffb6f6e6dc3288f740e9
mk2 <mk2@mk2.red>
2021-02-21 22:56:03 +0900

    fix: try

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import * as core from '@actions/core';
 
+interface Member {
+  id: string;
+  name: string;
+}
+
 async function run(): Promise<void> {
   try {
-    const zennAccounts: string = core.getInput('zenn');
-    core.debug(zennAccounts);
+    const zennAccounts: Member[] = JSON.parse(core.getInput('zenn'));
+    core.debug(JSON.stringify(zennAccounts));
     core.setOutput('zennAccounts', zennAccounts);
   } catch (error) {
     core.setFailed(error.message);
