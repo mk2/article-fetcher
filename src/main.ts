5327eb43a38b774ac9d23c908726f32d25d8d23b
mk2 <mk2@mk2.red>
2021-02-21 23:13:33 +0900

    try

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ interface Member {
 async function run(): Promise<void> {
   try {
     const zennAccounts: Member[] = JSON.parse(core.getInput('zenn'));
-    core.debug(JSON.stringify(zennAccounts));
+    core.info(JSON.stringify(zennAccounts));
     core.setOutput('zennAccounts', zennAccounts);
   } catch (error) {
     core.setFailed(error.message);
