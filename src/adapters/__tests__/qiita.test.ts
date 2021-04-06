b7662acb65fa2d2a48e130321b7b09ec561672ac
mk2 <mk2@mk2.red>
2021-04-06 23:56:49 +0900

    test: qiitaアダプターのテストケースを追加

--- src/adapters/__tests__/qiita.test.ts
+++ src/adapters/__tests__/qiita.test.ts
@@ -8,6 +8,15 @@ describe('qiita adapter', () => {
   beforeEach(async () => mockServer.start(3000));
   afterEach(async () => mockServer.stop());
 
+  test('ヘッダーにcontent-typeとcharsetが設定される', async () => {
+    const endpointMock = await mockServer.get('/qiita/testUser').thenReply(200, JSON.stringify([]));
+    await qiita.fetchArticles('testUser', 'testQiitaAccessToken');
+    const requests = await endpointMock.getSeenRequests();
+    expect(requests.length).toBe(1);
+    expect(requests[0].headers['content-type']).toBe('application/json');
+    expect(requests[0].headers['charset']).toBe('utf-8');
+  });
+
   test('トークンがある場合は、Authorizationヘッダーが設定される', async () => {
     const endpointMock = await mockServer.get('/qiita/testUser').thenReply(200, JSON.stringify([]));
     await qiita.fetchArticles('testUser', 'testQiitaAccessToken');
@@ -22,5 +31,11 @@ describe('qiita adapter', () => {
     const requests = await endpointMock.getSeenRequests();
     expect(requests.length).toBe(1);
     expect(requests[0].headers['authorization']).toBeUndefined();
+  });
+
+  test('レスポンスのステータスコードが200以外の場合、空配列が返る', async () => {
+    await mockServer.get('/qiita/testUser').thenReply(400, JSON.stringify([]));
+    const result = await qiita.fetchArticles('testUser', 'testQiitaAccessToken');
+    expect(result).toEqual([]);
   });
 });
